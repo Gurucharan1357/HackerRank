de68d053a5987668d9347512096b4933bc74a4be
Gurucharan1357 <165946548+Gurucharan1357@users.noreply.github.com>
2026-08-11 15:22:19 +0530

    Added solution for HackerRank challenge: occupations

---
+++ occupations.sql
@@ -0,0 +1,17 @@
+-- Problem: OCCUPATIONS
+-- Link: https://www.hackerrank.com/challenges/occupations/problem
+
+SELECT 
+    MAX(CASE WHEN Occupation = 'Doctor' THEN Name END) AS Doctor,
+    MAX(CASE WHEN Occupation = 'Professor' THEN Name END) AS Professor,
+    MAX(CASE WHEN Occupation = 'Singer' THEN Name END) AS Singer,
+    MAX(CASE WHEN Occupation = 'Actor' THEN Name END) AS Actor
+FROM (
+    SELECT 
+        Name, 
+        Occupation,
+        ROW_NUMBER() OVER (PARTITION BY Occupation ORDER BY Name) as 
+        row_num
+    FROM OCCUPATIONS
+) AS TempTable
+GROUP BY row_num;
